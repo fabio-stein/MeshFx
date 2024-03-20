7938454bb671f4790e2b5f58c9acd762562b7d5f
Fabio Stein <fabiolux999@gmail.com>
2024-03-20 18:05:44 +0000

    feat: simple winit browser

--- src/rust-core/glyphfx-winit/web_wrapper/src/lib.rs
+++ src/rust-core/glyphfx-winit/web_wrapper/src/lib.rs
@@ -1,5 +1,8 @@
-use wasm_bindgen::JsValue;
+use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen::prelude::wasm_bindgen;
+use winit::event::{Event, WindowEvent};
+use winit::keyboard::PhysicalKey;
+use winit::platform::web::WindowBuilderExtWebSys;
 
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
@@ -44,4 +47,50 @@ pub fn show_sum(ptr: i32) -> i32 {
 #[wasm_bindgen]
 pub fn simple_string() -> String {
     "Hello from Rust!".to_string()
+}
+
+#[wasm_bindgen]
+pub fn start_winit() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    let _window = winit::window::WindowBuilder::new()
+        .with_canvas(Some(canvas))
+        .build(&event_loop).unwrap();
+
+    let _ = event_loop.run(move |event, target| {
+        match event {
+            Event::WindowEvent { event, .. } => {
+                match event {
+                    WindowEvent::CloseRequested => {
+                    },
+                    WindowEvent::KeyboardInput { event, .. } => {
+                        let keycode_int = match event.physical_key {
+                            PhysicalKey::Code(keycode) => keycode as i32,
+                            PhysicalKey::Unidentified(_) => -1,
+                        };
+
+                        web_sys::console::log_1(&format!("Keycode: {}", keycode_int).into());
+                    },
+                    WindowEvent::MouseInput { state, button, .. } => {
+
+                    },
+                    WindowEvent::CursorMoved { position, .. } => {
+
+                    },
+                    WindowEvent::RedrawRequested => {
+                    },
+                    _ => {},
+                };
+            },
+            Event::Resumed => {
+            },
+            _ => {}
+        }
+    });
 }
